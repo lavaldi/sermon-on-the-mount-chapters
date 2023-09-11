a54d2549f816c2631f81037faefc508cb0b6fcc3
Claudia Valdivieso <claumavaldivieso@gmail.com>
2023-09-11 16:35:25 -0500

    update title

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Estudios Sobre el Sermón del Monte';
+export const SITE_TITLE = "El Sermón del Monte por D. Martyn Lloyd-Jones";
 export const SITE_DESCRIPTION = 'Estudios Sobre el Sermón del Monte por D. Martyn Lloyd-Jones';
